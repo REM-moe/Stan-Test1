43efe9500c6366c8941509fa77754580b9e3ef40
glitcchhh <diyaapeterr@gmail.com>
2024-04-01 22:54:27 +0530

    Initial analysis of article using gpt_logic

--- Brave_logic.py
+++ Brave_logic.py
@@ -1,7 +1,6 @@
-import os
 import requests
 from bs4 import BeautifulSoup
-count=1
+
 def fetch_article_content(url):
     try:
         response = requests.get(url)
@@ -32,25 +31,26 @@ def fetch_news(api_key, query, count=1, country='us', search_lang='en', spellche
         "spellcheck": spellcheck
     }
     response = requests.get(url, params=params, headers=headers)
+    count=1
     if response.status_code == 200:
         news_data = response.json()
         for item in news_data.get('results', []):
             article_url = item.get('url')
-            
             if article_url:
-                
                 content = fetch_article_content(article_url)
                 item['content'] = content
                 print(f"-------------------------{count}----------------------------")
                 count=count+1
                 print(content)                          #This will print the website content
+                # Write content to a text file
+                with open(f"{query}_article_{count}.txt", "w", encoding="utf-8") as file:
+                    file.write(content)
         return news_data
     else:
         print(f"Failed to fetch news data. Status code: {response.status_code}")
         return None
 
 # Example usage:
-brave_api_key = "BSA5FZ_coCBe04igRP0zZjqZBIvHudd"
-query = "Rock music"
-news_data = fetch_news(brave_api_key,query)
-#print(news_data)                                       # This will print the JSON response from the API
+api_key = "BSA5FZ_coCBe04igRP0zZjqZBIvHudd"             #Replace with the Brave Search api key
+query = "munich"
+news_data = fetch_news(api_key, query)
